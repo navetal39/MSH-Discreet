02f65faa6255adb2c10a4639259171260682e83e
Nave Tal <navetal@gmail.com>
2015-03-17 23:45:54 +0200

    Started working on target management
    It's supposed to not target the same island another fleet is targeting,
    but I don't remember if it worked or not. You know, elections and
    stuff...

--- bot_utils.py
+++ bot_utils.py
@@ -4,33 +4,63 @@ def patroll(game, pir):
 
     return (pir.id, 5-pir.id)
 
+
 def compare_islands(island_data1, island_data2):
     if island_data1[0] > island_data2[0]:
         return island_data2
     else:
         return island_data1
 
-def closest_island(game, pir, fleet, mission):
-    potential_islands_list = game.not_my_islands()
-    potential_locations = [i.location for i in potential_islands_list]
-
-    for ship in fleet:
-        if ship.location in potential_locations:
-            return ship.location
-        
-    enemy_islands = game.enemy_islands()
-    neutral_islands = game.neutral_islands()
-    if len(enemy_islands) == 0 and len(neutral_islands):
-        relevant_island_list = neutral_islands
-    elif len(enemy_islands) and len(neutral_islands) == 0:
-        relevant_island_list = enemy_islands
-    elif len(enemy_islands) == 0 and len(neutral_islands) == 0:
-        return patroll(game, pir)
+def compare_distances(game, pir1, pir2, target):
+    distance1 = game.distance(pir1, target)
+    distance2 = game.distance(pir2, target)
+    if distance1 <= distance2:
+        return 1
     else:
-        if mission == 'neutralize':
-            relevant_island_list = enemy_islands
+        return 2
+
+def closest_island(game, pir, fleet, mission, targeted = ''):
+    if pir is None: # If the fleet is dead
+        return (0,0), 'N/A'
+    
+    if targeted == '': # Normal scenario
+        
+        potential_islands_list = game.not_my_islands()
+        enemy_islands = game.enemy_islands()
+        neutral_islands = game.neutral_islands()
+        if len(enemy_islands) == 0 and len(neutral_islands):
+            relevant_islands_list = neutral_islands
+            actual_mission = 'capture'
+        elif len(enemy_islands) and len(neutral_islands) == 0:
+            relevant_islands_list = enemy_islands
+            actual_mission = 'neutralize'
         else:
-            relevant_island_list = neutral_islands
-    directions_list = [(game.distance(pir, i), i) for i in relevant_island_list]
-    closest_island_data = reduce(compare_islands, directions_list)
-    return closest_island_data[1]
+            actual_mission = mission
+            if mission == 'neutralize':
+                relevant_islands_list = enemy_islands
+            else:
+                relevant_islands_list = neutral_islands
+
+        #If already on an island - stay on it!
+        potential_locations = [i.location for i in potential_islands_list]
+        for ship in fleet:
+            if ship.location in potential_locations:
+                return ship.location
+            
+    else: # A collision happened
+        if mission == 'neutralize':
+            relevant_islands_list = game.enemy_islands()
+        elif mission == 'capture':
+            relevant_islands_list = game.neutral_islands()
+        else:
+            pass
+        relevant_islands_list.remove(targeted) # Remove the one that causes the conflict
+        actual_mission = mission
+        
+
+    distances_list = [(game.distance(pir, i), i) for i in relevant_islands_list]
+    if len(distances_list) == 0:
+        return patroll(game, pir), 'patroll'
+    else:
+        closest_island_data = reduce(compare_islands, distances_list)
+        return closest_island_data[1], actual_mission
--- my_bot.py
+++ my_bot.py
@@ -7,42 +7,57 @@ def do_turn(game):
     # Fleet creation:
     for n in xrange(5):
         pirate_n = game.get_my_pirate(n)
-        if n <= 2:
-            fleet1.append(pirate_n)
-        else:
-            fleet2.append(pirate_n)
+        if not pirate_n.is_lost:
+            if n <= 2:
+                fleet1.append(pirate_n)
+            else:
+                fleet2.append(pirate_n)
+
     # Leader election:
-    leader1 = fleet1[0]
-    leader2 = fleet2[0]
-    for pir in fleet1:
-        if pir.is_lost:
-            continue
-        else:
-            leader1 = pir
-    
-    for pir in fleet2:
-        if pir.is_lost:
-            continue
-        else:
-            leader2 = pir
+    try:
+        leader1 = fleet1[0]
+    except IndexError: # All the fleet is out!
+        leader1 = None
+    try:
+        leader2 = fleet2[0]
+    except IndexError: # All the fleet is out!
+        leader2 = None
 
-    #Target selection:
-    target1 = closest_island(game, leader1, fleet1, 'capture')
-    target2 = closest_island(game, leader2, fleet2, 'neutralize')
+    # Target selection:
+    target1, mission1 = closest_island(game, leader1, fleet1, 'capture')
+    target2, mission2 = closest_island(game, leader2, fleet2, 'neutralize')
 
-    if target1 = target2:
-        pass # To do: Prevent the 2 fleets from targeting the same island!
+    # Complete wipe-out prevention:
+    if len(fleet1) == 1: #Fleet in danger
+        target1 = leader1.initial_loc
+    elif len(fleet2) == 1: # Fleet indanger!
+        target2 = leader2.initial_loc
+        
+    # Collition prevention:
+    if target1 == target2:
+        target = target1
+        mission = mission1 # Just so there won't be any confusion
+        if mission != 'N/A':
+            closer_fleet = compare_distances(game, leader1, leader2, target)
+            if closer_fleet == 1:
+                target2, new_mission = closest_island(game, leader2, fleet2, mission, target)
+            else:
+                target1, new_mission = closest_island(game, leader1, fleet1, mission, target)
 
-    #Seting sail...etion?
+    # Seting sail...etion?
     for pir in fleet1:
         try:
             direction = game.get_directions(pir, target1)
+            game.debug(1)
             game.set_sail(pir, direction[0])
+            game.debug(2)
         except Exception, e:
             game.debug(e)
     for pir in fleet2:
         try:
             direction = game.get_directions(pir, target2)
+            game.debug(3)
             game.set_sail(pir, direction[0])
+            game.debug(4)
         except Exception, e:
             game.debug(e)
