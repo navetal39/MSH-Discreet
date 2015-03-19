a9b87d10e6cbbe3246d7c23c987c9435570fabf6
Nave Tal <navetal@gmail.com>
2015-03-20 00:21:17 +0200

    removed util
    util is useless.

--- saars_demo_bot/util.py
+++
@@ -1,7 +0,0 @@
-def reverse_direction(game, direction):
-    changes = {'n':'s', 's':'n', 'e':'w', 'w':'e', '-':'-'}
-    try:
-        return changes[direction]
-    except Exception, e:
-        game.debug('An error occured: '+e)
-        return '-' # Default value
