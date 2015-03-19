944df1507ea8ff9e26a38956c32285553ae5dc7f
KatzSaar <mailforgames.saar@gmail.com>
2015-03-19 18:46:19 +0200

    Revert "Revert "Added my bot.""
    This reverts commit acb63a55d8708223a39df4dc0cabef725ff7a931.

---
+++ Bot_cs/Bot/Utils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Pirates;
+
+namespace PirateBot
+{
+    class Utils
+    {
+        public static Direction NegDirec(Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.CLOAK:
+                    return Direction.REVEAL;
+                case Direction.EAST:
+                    return Direction.WEST;
+                case Direction.NORTH:
+                    return Direction.SOUTH;
+                case Direction.NOTHING:
+                    return Direction.NOTHING;
+                case Direction.REVEAL:
+                    return Direction.CLOAK;
+                case Direction.SOUTH:
+                    return Direction.NORTH;
+                case Direction.WEST:
+                    return Direction.EAST;
+                default:
+                    return Direction.NOTHING;
+            }
+        }
+    }
+}
